0ed47331564875ee8e73caa5727779e849d4b091
ngodat0103 <ngovuminhdat@gmail.com>
2025-03-18 15:36:23 +0700

    commit-latest

--- tf/main.tf
+++ tf/main.tf
@@ -4,8 +4,13 @@ locals {
 
 
 module "init_storage_and_images" {
-  source = "./common-stuff/kvm/images"
+  source = "./common-stuff/kvm/images/ubuntu-server-2204"
 }
+module "ubuntu_desktop_iso" {
+  source = "./common-stuff/kvm/images/ubuntu-desktop-2204"
+}
+
+
 
 
 module "network" {
@@ -18,7 +23,7 @@ module "network" {
 
 
 module "squid_proxy_server" {
-  source             = "./common-stuff/kvm/vm"
+  source             = "./common-stuff/kvm/vm/ubuntu-server-2204"
   vm_name            = "squid-proxy"
   vm_memory          = 1024
   vm_vcpu            = 2
@@ -26,19 +31,31 @@ module "squid_proxy_server" {
   vm_network_address = ["192.168.99.10"]
   username           = "ubuntu"
   ssh_key            = file("~/.ssh/id_rsa.pub")
-  pool_id            = module.init_storage_and_images.pool_name
+  pool_name            = module.init_storage_and_images.pool_name
   base_volume_id     = module.init_storage_and_images.ubuntu_2204_disk_id
 }
 module "icap_server" {
-  source             = "./common-stuff/kvm/vm"
+  source             = "./common-stuff/kvm/vm/ubuntu-server-2204"
   vm_name            = "icap-server"
   vm_memory          = 4096
   vm_vcpu            = 2
   ssh_key            = file("~/.ssh/id_rsa.pub")
   vm_network_name    = local.network_name
   vm_network_address = ["192.168.99.20"]
-  pool_id            = module.init_storage_and_images.pool_name
+  pool_name            = module.init_storage_and_images.pool_name
   base_volume_id     = module.init_storage_and_images.ubuntu_2204_disk_id
+}
+
+
+module "client" {
+  source = "./common-stuff/kvm/vm/ubuntu-desktop-2204"
+  vm_name = "client"
+  vm_memory = 4096
+  vm_vcpu = 2
+  vm_network_name = local.network_name
+  vm_network_address = ["192.168.99.30"]
+  hashed_password = "$5$hRSXIy7qauenKl0K$cm53Nh9aeyIvs9/JCF.F8jrS29viTFCLc1f7pZetfW8"
+  iso_volume_id = module.ubuntu_desktop_iso.id
 }
 
 output "squid_proxy_server_ip" {
